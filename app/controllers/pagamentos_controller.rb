c25f6ecd7bfcfb2eaa61b1f20357c1c89f774206
LuisFelipePereiraDaSilva <luisfelipepereira150@gmail.com>
2019-06-11 16:47:30 -0300

    Foi atualizado a view de pagamento com o modelo padrao de tela do sistema e com as tabelas de clientes e contas e o formulario de registrar um pagamento

--- app/controllers/pagamentos_controller.rb
+++ app/controllers/pagamentos_controller.rb
@@ -1,4 +1,30 @@
 class PagamentosController < ApplicationController
+
   def index
+    if(params[:pesquisa] &&  params[:pesquisa] != '')
+      @clientes = Cliente.pesquisa(params[:pesquisa])
+    else
+      @clientes = Cliente.listaClientes
+    end
+
+    @contasDevendo = [];
+    @contasAtrasadas = [];
+    @cliente = Cliente.new
+  end
+
+  def show
+    carregarContas(params[:id])
+    @cliente = Cliente.find(params[:id])
+    render 'pagamentos/index'
+  end
+
+
+  private
+
+  def carregarContas id
+    contas = Contum.listaContasCliente(id)
+    @contasDevendo = contas.listaContasDevendo
+    @contasAtrasadas = contas.listaContasAtrasadas
+    @clientes = Cliente.listaClientes
   end
 end
