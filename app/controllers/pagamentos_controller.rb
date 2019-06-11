4749db3c078c02efabbfab2e2cb2a91cb6e3219f
LuisFelipePereiraDaSilva <luisfelipepereira150@gmail.com>
2019-06-11 16:59:49 -0300

    Foi feito o algoritmo de registrar o pagamento do cliente

--- app/controllers/pagamentos_controller.rb
+++ app/controllers/pagamentos_controller.rb
@@ -1,4 +1,5 @@
 class PagamentosController < ApplicationController
+  respond_to :js, :html
 
   def index
     if(params[:pesquisa] &&  params[:pesquisa] != '')
@@ -18,8 +19,105 @@ class PagamentosController < ApplicationController
     render 'pagamentos/index'
   end
 
+  def create
+    cpf = params[:cpf]
+    valorContas = params[:valorContas]
+    valorCliente = params[:valorCliente]
+    cliente = nil
+    if (cliente = validarDadosPagamento cpf, valorContas, valorCliente) != nil
+      valorContas = valorContas.to_f
+      valorCliente = valorCliente.to_f
+      pagamentoCaso1 params[:idsContasDevendo], params[:idsContasAtrasada], valorCliente, valorContas
+      pagamentoCaso2 params[:idsContasDevendo], params[:idsContasAtrasada], valorCliente, valorContas
+      carregarContas cliente.id
+    end
+  end
 
   private
+  # Caso quando o valor dado pelo cliente é maior ou igual do que o valor das contas selecionadas
+  def pagamentoCaso1 idsContasDevendo, idsContasAtrasada, valorCliente, valorContas
+    if valorCliente >= valorContas
+      contas = idsContasAtrasada + idsContasDevendo
+      contas = contas.split('-')
+      contas.each do |conta|
+        if conta != ""
+          conta = Contum.find(conta)
+          conta.update(status: "Paga", dataPagamento: Time.now.to_s)
+        end
+      end
+      @resultadoPagamento = "Pagamento Efetuado: Troco = " + (valorCliente - valorContas).to_s;
+    end
+  end
+
+  # Caso quando o valor dado pelo cliente é menor do que o valor das contas selecionadas
+  def pagamentoCaso2 idsContasDevendo, idsContasAtrasada, valorCliente, valorContas
+    if valorCliente < valorContas
+      valorPago = 0
+      contas = idsContasAtrasada + idsContasDevendo
+      contas = contas.split('-')
+      contas.each do |conta|
+        if conta != ""
+          if valorCliente > valorPago
+            conta = Contum.find(conta)
+            valor = conta.valor
+            if conta.juros > 0
+              valor += (conta.juros/100) * valor;
+            end
+            if (valorCliente - valorPago) >= valor
+              conta.update(status: "Paga", dataPagamento: Time.now)
+            else
+              valor -= (valorCliente - valorPago)
+              conta.update(valor: valor)
+            end
+            valorPago += valor
+          end
+        end
+      end
+      @resultadoPagamento = "restante-Pagamento Efetuado: Restante = " + (valorContas - valorCliente).to_s;
+    end
+  end
+
+  def validarDadosPagamento cpf, valorContas, valorCliente
+    resultado = nil
+    if verificarVazioOuNulo(cpf) || cpf.length != 11 || !verificarNumeroInt(cpf)
+      @resultadoPagamento = "erro-Preenchar um cpf válido"
+    elsif verificarVazioOuNulo valorContas || valorContas.to_s == '0' || !verificarNumeroFloat(valorContas)
+      @resultadoPagamento = "erro-Nenhuma conta foi selecionada"
+    elsif verificarVazioOuNulo valorCliente || !verificarNumeroFloat(valorCliente)
+      @resultadoPagamento = "erro-Preencha o valor dado pelo cliente"
+    elsif (resultado = Cliente.buscarClientePorCpf cpf) == nil
+      @resultadoPagamento = "erro-Esse cliente não existe"
+    end
+    resultado
+  end
+
+  def verificarVazioOuNulo string
+    resultado = false
+    if string == nil or string == ''
+      resultado = true
+    end
+    resultado
+  end
+
+  def verificarNumeroInt string
+    resultado = true
+    begin
+      string = string.to_i
+    rescue
+      resultado = false
+    end
+    resultado
+  end
+
+  def verificarNumeroFloat string
+    resultado = true
+    begin
+      string = string.to_f
+    rescue
+      resultado = false
+    end
+    resultado
+  end
 
   def carregarContas id
     contas = Contum.listaContasCliente(id)
