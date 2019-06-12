d5baee87835095de6bd07e9898d9d7d180329e5c
gabriel-gf <gabriel123scc@gmail.com>
2019-06-11 22:33:07 -0300

    Implementação de mais 3 testes model de contas

--- test/models/contum_test.rb
+++ test/models/contum_test.rb
@@ -27,5 +27,30 @@ class ContaTest < ActiveSupport::TestCase
     assert_not conta.save
   end
 
+  test 'Criar conta com valor conta negativo' do
+    cliente = Cliente.new nome:'luis', cpf:'11304353427', celular:'999999999', email:'felipe@gmail.com', senha:'12345', cidade:'garanhuns', rua:'centro', numero:'0'
+    cliente.save
+    funcionario = Funcionario.new nome: 'jose', cpf:'11111111111', celular:'999999999', email:'jose@gmail.com', senha:'123456789', cidade:'garanhuns', rua:'centro', numero:'0', cargo:'caixa'
+    funcionario.save
+    conta = Contum.new cliente: cliente.id, funcionario: funcionario.id, valor:-100, juros: 2
+    assert_not conta.save
+  end
 
+  test 'Criar conta sem juros' do
+    cliente = Cliente.new nome:'luis', cpf:'11304353427', celular:'999999999', email:'felipe@gmail.com', senha:'12345', cidade:'garanhuns', rua:'centro', numero:'0'
+    cliente.save
+    funcionario = Funcionario.new nome: 'jose', cpf:'11111111111', celular:'999999999', email:'jose@gmail.com', senha:'123456789', cidade:'garanhuns', rua:'centro', numero:'0', cargo:'caixa'
+    funcionario.save
+    conta = Contum.new cliente: cliente.id, funcionario: funcionario.id, valor:100
+    assert_not conta.save
+  end
+
+  test 'Criar conta sem valor de conta' do
+    cliente = Cliente.new nome:'luis', cpf:'11304353427', celular:'999999999', email:'felipe@gmail.com', senha:'12345', cidade:'garanhuns', rua:'centro', numero:'0'
+    cliente.save
+    funcionario = Funcionario.new nome: 'jose', cpf:'11111111111', celular:'999999999', email:'jose@gmail.com', senha:'123456789', cidade:'garanhuns', rua:'centro', numero:'0', cargo:'caixa'
+    funcionario.save
+    conta = Contum.new cliente: cliente.id, funcionario: funcionario.id, juros: 2
+    assert_not conta.save
+  end
 end
