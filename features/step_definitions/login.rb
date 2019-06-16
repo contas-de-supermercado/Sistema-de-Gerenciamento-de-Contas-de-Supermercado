05cef7e2dbe049b18bcc0ad2f7e1b0025c34f36c
LuisFelipePereiraDaSilva <luisfelipepereira150@gmail.com>
2019-06-16 13:26:53 -0300

    Atualizacao do module Login nos cenarios do cucumber

--- features/step_definitions/login.rb
+++ features/step_definitions/login.rb
@@ -1,19 +1,3 @@
-module Login
-  def logarComoFuncionario
-    Pessoa.setPessoaLogada(Pessoa.new nome:"teste", cpf:"00000000000", telefone:"00000000000", celular:"00000000000",
-                                      email:"teste@gmail.com", senha:"teste", cidade:"-----", rua:"-----", numero:"-----", cargo:"-----",
-                                      tipo:1)
-  end
-
-  def logarComoCliente
-    Pessoa.setPessoaLogada(Pessoa.new nome:"teste", cpf:"00000000000", telefone:"00000000000", celular:"00000000000",
-                                      email:"teste@gmail.com", senha:"teste", cidade:"-----", rua:"-----", numero:"-----", cargo:"-----",
-                                      tipo:0)
-  end
-end
-
-World Login
-
 Given("Eu abro a pagina inicial do sistema") do
   visit '/logins'
 end
