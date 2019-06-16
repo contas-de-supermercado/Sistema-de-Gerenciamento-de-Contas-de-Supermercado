16eee912fb4df1ebe52b246553711094efbccade
LuisFelipePereiraDaSilva <luisfelipepereira150@gmail.com>
2019-06-16 18:59:41 -0300

    Atualizacoes das validacoes de cpf e email de um cliente ou funcionario

--- app/models/pessoa.rb
+++ app/models/pessoa.rb
@@ -1,21 +1,40 @@
 class Pessoa < ApplicationRecord
+  validates_with ValidacoesAuxPessoa
   validates :nome, presence: {message: " nao pode ser em branco" },
             length: { minimum: 3, too_short: "deve ter pelo menos 3 caracteres" }
   validates :cpf, presence: {message: " nao pode ser em branco" },
             length: { is: 11 , message: "deve ter 11 caracteres"},
-            uniqueness: {message: " nao pode ser repetido" },
             numericality: {message: "deve ser um numero"}
   validates :senha, presence: {message: " nao pode ser em branco" },
             length: {minimum: 5,too_short: "deve ter pelo menos 5 caracteres" }
   validates :email, presence: {message: " nao pode ser em branco" },
-            format: { with: URI::MailTo::EMAIL_REGEXP, message: "so permite emails validos" },
-            uniqueness: {message: " nao pode ser repetido" }
+            format: { with: URI::MailTo::EMAIL_REGEXP, message: "so permite emails validos" }
   validates :cidade, presence: {message: " nao pode ser em branco" }
   validates :rua, presence: {message: " nao pode ser em branco" }
   validates :numero, presence: {message: " nao pode ser em branco" }
   validates :celular, presence: {message: " nao pode ser em branco" },
             length: { in: 9..11 , message: " numero com tamanho incorreto"},
             numericality: {message: "deve ser um numero"}
+
+  def self.verificarExisteCpfAtivo pessoa
+    pessoas = Cliente.listaClientes
+    pessoas.each do |pessoaAux|
+      if pessoaAux.cpf == pessoa.cpf && pessoaAux.id != pessoa.id
+        return pessoa
+      end
+    end
+    return nil
+  end
+
+  def self.verificarExisteEmailAtivo pessoa
+    pessoas = Cliente.listaClientes
+    pessoas.each do |pessoaAux|
+      if pessoaAux.email == pessoa.email && pessoaAux.id != pessoa.id
+        return pessoa
+      end
+    end
+    return nil
+  end
 
   @@pessoaLogada = nil
 
